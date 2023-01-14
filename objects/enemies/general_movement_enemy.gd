292385816c792e062d66c0740be8999ce95ce8b6
电童家的Fuaisam <22123964+Lazy-Rabbit-2001@users.noreply.github.com>
2023-01-14 12:21:29 +0800

    updated goomba to sync with gravity_body_2d.gd

--- objects/enemies/general_movement_enemy.gd
+++ objects/enemies/general_movement_enemy.gd
@@ -2,12 +2,13 @@ extends GravityBody2D
 
 @export_category("GeneralMovementEnemy")
 @export var look_at_player: bool = false
+@export var rigid_movement: bool = false
 
 
 func _ready() -> void:
 	if look_at_player && Thunder._current_player:
-		velocity_local.x *= global_transform.affine_inverse().basis_xform(global_position.direction_to(Thunder._current_player.global_position).sign()).x
+		speed.x *= global_transform.affine_inverse().basis_xform(global_position.direction_to(Thunder._current_player.global_position).sign()).x
 
 func _physics_process(delta: float) -> void:
 	gravity_process(Thunder.get_delta(delta))
-	motion_process(Thunder.get_delta(delta),false)
+	motion_process(Thunder.get_delta(delta),rigid_movement)
