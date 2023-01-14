ecde4472bc4bef4dc18182a8363734a95fa93dbd
电童家的Fuaisam <22123964+Lazy-Rabbit-2001@users.noreply.github.com>
2023-01-14 13:51:13 +0800

    updated goomba

--- objects/enemies/general_movement_enemy.gd
+++ objects/enemies/general_movement_enemy.gd
@@ -10,5 +10,5 @@ func _ready() -> void:
 		speed.x *= global_transform.affine_inverse().basis_xform(global_position.direction_to(Thunder._current_player.global_position).sign()).x
 
 func _physics_process(delta: float) -> void:
-	gravity_process(Thunder.get_delta(delta))
-	motion_process(Thunder.get_delta(delta),rigid_movement)
+	gravity_process()
+	motion_process(Thunder.get_delta(delta),true,false,rigid_movement)
