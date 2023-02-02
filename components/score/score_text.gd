9a0beef769cad2bf2c4214298bebf849dc7f29df
reflexguru <trianglestudioad@gmail.com>
2023-02-02 09:23:03 +0300

    tweak scoretext

--- components/score/score_text.gd
+++ components/score/score_text.gd
@@ -6,11 +6,11 @@ func _init(string: String, ref: Node2D):
 	Thunder._current_stage.add_child(self)
 	var pos = size / 2
 	global_position = ref.global_position - pos
-	# Разрабы годота ДОЛБАЕБЫ
+	# godot developers forgot to implement global_rotation in control nodes :skull:
 	#global_rotation = Thunder._current_player.global_rotation
 	
 	var tw = get_tree().create_tween()
-	tw.tween_property(self, "global_position:y", global_position.y - 64, 1.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	tw.tween_property(self, "global_position:y", global_position.y - 48, 1.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	
 	var timer = get_tree().create_timer(3.0, false, true)
 	timer.timeout.connect(func():
