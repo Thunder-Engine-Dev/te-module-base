f21062144e3ca5d3c0c65af3e3c97b6895be9d4b
Lazy-Rabbit-2001 <2733679597@qq.com>
2023-02-03 14:02:20 +0800

    stomping sound and some fixes

--- objects/enemies/_dead/enemy_stomped.gd
+++ objects/enemies/_dead/enemy_stomped.gd
@@ -7,15 +7,15 @@ var destroying: bool
 @onready var collsion_box:CollisionShape2D = $Collision
 
 
-func _process(delta: float) -> void:
-	if !destroying: return
-	
-	modulate.a -= 0.1
-	if modulate.a <= 0: queue_free()
-
 func _ready() -> void:
 	for i in get_children():
 		if i is Sprite2D:
 			collsion_box.shape.size = i.texture.get_size()
 		elif i is AnimatedSprite2D:
 			collsion_box.shape.size = i.sprite_frames.get_frame_texture(i.animation,i.frame).get_size()
+
+func _process(delta: float) -> void:
+	if !destroying: return
+	
+	modulate.a -= 0.1 * Thunder.get_delta(delta)
+	if modulate.a <= 0: queue_free()
