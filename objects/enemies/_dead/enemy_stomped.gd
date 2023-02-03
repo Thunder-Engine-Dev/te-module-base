ea039341766026f1a31ff6cdef2a003da3435594
Lazy-Rabbit-2001 <2733679597@qq.com>
2023-02-03 13:47:41 +0800

    stomped enemy's fading out

--- objects/enemies/_dead/enemy_stomped.gd
+++ objects/enemies/_dead/enemy_stomped.gd
@@ -1,7 +1,17 @@
 extends GeneralMovementBody2D
 
+@export var disappearing_strength:float = 0.1
+
+var destroying: bool
+
 @onready var collsion_box:CollisionShape2D = $Collision
 
+
+func _process(delta: float) -> void:
+	if !destroying: return
+	
+	modulate.a -= 0.1
+	if modulate.a <= 0: queue_free()
 
 func _ready() -> void:
 	for i in get_children():
