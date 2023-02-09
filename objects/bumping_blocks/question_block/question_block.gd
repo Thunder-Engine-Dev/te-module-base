d5653f05eec9ea32856c0e020c76ed3fe8d2a73e
jue131 <83157668+jue131@users.noreply.github.com>
2023-02-09 16:48:52 +0900

    temp bumping detection for question blocks

--- objects/bumping_blocks/question_block/question_block.gd
+++ objects/bumping_blocks/question_block/question_block.gd
@@ -1,2 +1,20 @@
 @icon("res://modules/base/objects/bumping_blocks/question_block/textures/icon.png")
 extends StaticBumpingBlock
+
+func _physics_process(delta):
+	var player = Thunder._current_player
+	
+	if player.is_on_ceiling() && !triggered:
+		for i in player.get_slide_collision_count():
+			var slide = player.get_slide_collision(i)
+			
+			var collider
+			if !slide.has_method(&"get_collider"): continue
+			collider = slide.get_collider()
+			
+			print(collider, i)
+			if collider == self:
+				triggered = true
+				bump(true)
+				$AnimatedSprite2D.animation = &"empty"
+				continue
