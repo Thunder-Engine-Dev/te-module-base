8ce8bea4931e25653f67ae3af2427e2cf82ee452
Lazy-Rabbit-2001 <2733679597@qq.com>
2023-02-10 15:51:05 +0800

    implemented bump block @tool mode

--- objects/bumping_blocks/question_block/question_block.gd
+++ objects/bumping_blocks/question_block/question_block.gd
@@ -1,7 +1,10 @@
 @icon("res://modules/base/objects/bumping_blocks/question_block/textures/icon.png")
+@tool
 extends StaticBumpingBlock
 
 func _ready() -> void:
+	if Engine.is_editor_hint(): return
+	
 	super()
 	
 	if area_below: area_below.body_entered.connect(_detect_below)
@@ -13,10 +16,9 @@ func _detect_below(body) -> void:
 	if triggered: return
 	
 	if body is Player:
-		if body.velocity_local.y < body.config.max_fall_speed:
+		if body.velocity_local.y <= 0 && !body.is_on_floor():
 			call_bump()
 			return
-	
 
 func call_bump() -> void:
 	bump(true)
