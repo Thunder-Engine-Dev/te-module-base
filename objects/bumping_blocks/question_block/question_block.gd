052e6fbaadecc0aa716e4f5e9f04e30785213523
jue131 <83157668+jue131@users.noreply.github.com>
2023-02-10 13:48:53 +0900

    Better collisions with question block

--- objects/bumping_blocks/question_block/question_block.gd
+++ objects/bumping_blocks/question_block/question_block.gd
@@ -1,20 +1,25 @@
 @icon("res://modules/base/objects/bumping_blocks/question_block/textures/icon.png")
 extends StaticBumpingBlock
 
-func _physics_process(delta):
-	var player = Thunder._current_player
+func _ready() -> void:
+	super()
 	
-	if player.is_on_ceiling() && !triggered:
-		for i in player.get_slide_collision_count():
-			var slide = player.get_slide_collision(i)
-			
-			var collider
-			if !slide.has_method(&"get_collider"): continue
-			collider = slide.get_collider()
-			
-			print(collider, i)
-			if collider == self:
-				triggered = true
-				bump(true)
-				$AnimatedSprite2D.animation = &"empty"
-				continue
+	if area_below: area_below.body_entered.connect(_detect_below)
+
+#func _physics_process(delta):
+#	super(delta)
+
+func _detect_below(body) -> void:
+	if triggered: return
+	
+	if body is Player:
+		if body.velocity_local.y < body.config.max_fall_speed:
+			call_bump()
+			return
+	
+
+func call_bump() -> void:
+	bump(true)
+	$AnimatedSprite2D.animation = &"empty"
+
+
