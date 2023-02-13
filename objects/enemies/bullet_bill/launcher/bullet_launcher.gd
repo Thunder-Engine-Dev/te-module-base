661a49ca2ddb4b3cd844b0ba29c97eaf48ec232a
Lazy-Rabbit-2001 <2733679597@qq.com>
2023-02-13 15:49:29 +0800

    prepared bullet bill launcher

---
+++ objects/enemies/bullet_bill/launcher/bullet_launcher.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+@export_category("BulletBillLauncher")
+@export_group("Bullet")
+@export var bullet_bill:Node2DCreation
+@export_group("Delay")
+@export var first_shooting: float
+@export var shooting_delay_min: float
+@export var shooting_delay_max: float
+
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var launcher: Sprite2D = $Launcher
+@onready var base:Control = $Base
+
+
+func _ready() -> void:
+	collision_shape.position.y = base.get_rect().get_center().y
+	collision_shape.shape.size.y = base.get_rect().size.y
