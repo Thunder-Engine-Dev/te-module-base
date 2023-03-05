cdb2dbe641bf2bf5292aa208e55ad7ef38f02c98
reflexguru <trianglestudioad@gmail.com>
2023-03-05 19:38:56 +0300

    add super() to enemy_stomped & fix a typo

--- objects/enemies/_dead/enemy_stomped.gd
+++ objects/enemies/_dead/enemy_stomped.gd
@@ -4,15 +4,17 @@ extends GeneralMovementBody2D
 
 var destroying: bool
 
-@onready var collsion_box:CollisionShape2D = $Collision
+@onready var collision_box:CollisionShape2D = $Collision
 
 
 func _ready() -> void:
+	super()
+	
 	for i in get_children():
 		if i is Sprite2D:
-			collsion_box.shape.size = i.texture.get_size()
+			collision_box.shape.size = i.texture.get_size()
 		elif i is AnimatedSprite2D:
-			collsion_box.shape.size = i.sprite_frames.get_frame_texture(i.animation,i.frame).get_size()
+			collision_box.shape.size = i.sprite_frames.get_frame_texture(i.animation,i.frame).get_size()
 
 func _process(delta: float) -> void:
 	if !destroying: return
